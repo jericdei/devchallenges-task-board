6fab3587f537a8b8224c2d6da88ed02c6bfa130b
Jeric June Logan <loganjeric25@gmail.com>
2024-09-22 11:53:33 +0800

    typography classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Logo from "@/components/vector/logo";
+
 export default function Home() {
   return (
-    <div>
-      <p>nice</p>
-    </div>
+    <main className="min-h-screen flex justify-center items-center">
+      <div className="p-8">
+        <div className="flex items-center gap-2">
+          <Logo />
+          <h1>My Task Board</h1>
+        </div>
+      </div>
+    </main>
   );
 }
